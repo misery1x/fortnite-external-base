dfbf4cdb79cb7c281d9c91e5130391766d10e256
misery1x <brisonbuilder123@proton.me>
2025-07-29 19:48:03 -0400

    Update offsets.hpp

--- fn-base/game/utility/offsets.hpp
+++ fn-base/game/utility/offsets.hpp
@@ -5,21 +5,21 @@
 namespace offsets
 {
 	// Cache
-	inline uintptr_t UWorld = 0x17AA2698;
-	inline uintptr_t OwningGameInstance = 0x238;
+	inline uintptr_t UWorld = 0x173AF320;
+	inline uintptr_t OwningGameInstance = 0x250;
 	inline uintptr_t LocalPlayers = 0x38;
 	inline uintptr_t PlayerController = 0x30;
 	inline uintptr_t AcknowledgedPawn = 0x350;
 	inline uintptr_t Mesh = 0x328;
-	inline uintptr_t PlayerState = 0x2B0;
-	inline uintptr_t TeamIndex = 0x1259;
+	inline uintptr_t PlayerState = 0x2C8;
+	inline uintptr_t TeamIndex = 0x1291;
 	inline uintptr_t RootComponent = 0x1B0;
-	inline uintptr_t GameState = 0x1C0;
-	inline uintptr_t PlayerArray = 0x2c0;
+	inline uintptr_t GameState = 0x1D8;
+	inline uintptr_t PlayerArray = 0x2C0;
 	inline uintptr_t PawnPrivate = 0x320;
 
 	// Getbone
-	inline uintptr_t BoneArray = 0x5E8;
-	inline uintptr_t BoneCache = 0x5F8;
+	inline uintptr_t BoneArray = 0x5e8;
+	inline uintptr_t BoneCache = 0x5d8;
 	inline uintptr_t ComponentToWorld = 0x1e0;
 }
